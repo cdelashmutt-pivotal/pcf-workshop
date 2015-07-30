518e466157916ec79137806beea7951f61efc17d
Chris DeLashmutt <cdelashmutt@pivotal.io>
2015-07-29 22:53:12 -0400

    Adding script to apply security

--- set-up/setup-class.ps1
+++ set-up/setup-class.ps1
@@ -1,9 +1,33 @@
-﻿Foreach ($email in gc class-participant-emails.txt){
+﻿<#
+    .SYNOPSIS 
+      Creates a set of CF organizations and users based on an input file.
+    .EXAMPLE
+     Get-WmiClasses -class disk -ns root\cimv2"
+     This command finds wmi classes that contain the word disk. The 
+     classes returned are from the root\cimv2 namespace.
+#>
 
-    $split = $email.Split('_')
-    $substring = $split.Substring(0,1)
-    $org = -join $substring + '-org'
-    "INFO: Setting up $($org): for $($email)"
+param(
+    [switch]$NoBindSecurityGroup,
+    [string]$EmailFile = "class-participant-emails.txt",
+    [switch]$UseEntireEmail
+)
+
+Foreach ($email in gc $EmailFile){
+
+    Write-Host "Processing $email"
+    if($UseEntireEmail)
+    {
+        $split = $email.Split("@")[0]
+        $org = $split + '-org'
+    }
+    else
+    {
+        $split = $email.Split('_')
+        $substring = $split.Substring(0,1)
+        $org = $substring + '-org'
+    }
+    Write-Host "INFO: Setting up $($org): for $($email)"
     cf create-org $org
     cf create-space development -o $org
     cf create-space production -o $org
@@ -15,5 +39,7 @@
     cf set-space-role $email $org development SpaceDeveloper
     cf set-space-role $email $org production SpaceManager
     cf set-space-role $email $org production SpaceDeveloper
-    cf bind-security-group all_open $org production
+    if( -Not $NoBindSecurityGroup ) {
+        cf bind-security-group all_open $org production
+    }
 }
