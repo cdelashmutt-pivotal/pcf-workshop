b870228886c1246443d393467fccac2bfdd52898
Chris DeLashmutt <cdelashmutt@pivotal.io>
2015-10-30 09:54:46 -0400

    simplifying cleanup script

--- set-up/cleanup-class.sh
+++ set-up/cleanup-class.sh
@@ -2,7 +2,7 @@
 
 for email in `cat class-participant-emails.txt` ; do
 
-	org=`echo $email | tr '_' "\n" | colrm 2 | tr -d "\n" && echo -org`
+	org=`echo $email | sed 's/\([^@]*\).*/\1-org/'`
 	echo "INFO: Cleaning up $org: for $email"
 
 	cf delete-org $org -f 
